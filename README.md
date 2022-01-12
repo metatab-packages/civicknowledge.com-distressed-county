b85bce87a244e007a96655ff82fd4282e9bbace5
Eric Busboom <eric@civicknowledge.com>
2022-01-12 15:11:01 -0800

    .

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ variables](https://www.ffiec.gov/censusapp.htm) and flags indicating ways in
 which housing and populations in tracts are distressed. This dataset aggregates
 those tract flags into counts of distressed tracts per county.
 
+
+## Processing
+
+The source dataset is ``ffeic_distressed``, which extracts a subset of the columns from the FFEIC Census Flat File, which is itself an extract from the American Community Survey, with a few extra columns for things like ratios of income of a tract to the containing MSA. This data set aggreates the ``ffeic_distressed`` to counties, and produces for each of the source dataset flags: 
+
+* A count of tracts in the county with the flag set.
+* The sum of the populatinos of trats with the flag set
+* The percentage of the county's tracts with the flag set
+* The percentage of the countys' population with the flag set. 
--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@
 
 # You can also create you own tasks
 from invoke import task
+import metapack as mp
 
 from metapack_build.tasks.package import ns
 
@@ -29,9 +30,42 @@ from metapack_build.tasks.package import ns
 
 
 @task
-def example_task(c):
-    """An exmaple Invoke task"""
-    c.run("echo 'this is an example task' ")
+def set_descriptions(c):
+    """Set the descriptions of the columns from the upstram data source"""
+    
+    pkg = mp.open_package('.')
+    r = pkg.resource('distressed_counties')
+    
+    
+    ur = pkg.reference('ffeic_distressed') # Upstream resource
+    upkg = ur.resolved_url.package_url.doc # Upstrem package
+    
+    ucols = upkg.resource(ur.resource.name).columns()
+    
+    dmap = { c['header']:c['description'] for c in ucols}
+    
+    def get_desc(h):
+        for k,v in dmap.items():
+            if h.startswith(k):
+                
+                if '_pop_pct' in h:
+                    return "Percentage of population in county in tracts with flag: "+v
+                elif '_pop' in h:
+                    return "Population in county in tracts with flag: "+v
+                elif '_pct' in h:
+                    return "Percentage of tracts in county with flag: "+v
+                else:
+                    return "Count of tracts in county with flag: "+v
+             
+    
+    for c in r.schema_term.find('Table.Column'):
+        desc = get_desc(c.name)
+        
+        if desc and not c['description']:
+            c['description'] = desc
+        
+    
+    pkg.write()
+    
 
-
-ns.add_task(example_task)
+ns.add_task(set_descriptions)
